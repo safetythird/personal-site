56d29c8128f96b8d8aca860a3cf8692a5c5099d7
Ben Greenberg <bgreenberg85@gmail.com>
2023-02-04 09:46:37 -0800

    Add bundle plugin to config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const pluginBundle = require("@11ty/eleventy-plugin-bundle");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginBundle);
   return {
     dir: {
       input: "src"
